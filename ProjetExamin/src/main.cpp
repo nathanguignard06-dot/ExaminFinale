29df83076a6850e3e14b9554d44039a52fd0320a
Nathan Guignard <nathanguignard06@gmail.com>
2026-04-07 10:21:59 -0300

    Ajout des requettes api a partir du microcontroleur et non de la page car serveur block cors, aussi petite modification de la page html pour afficher les donnees

--- ProjetExamin/src/main.cpp
+++ ProjetExamin/src/main.cpp
@@ -3,6 +3,8 @@
 #include <FS.h>
 #include <SPIFFS.h>
 #include <WebServer.h>
+#include <HTTPClient.h>  // ← AJOUTE CET INCLUDE
+#include <ArduinoJson.h> // Pour parser JSON facilement
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -16,8 +18,72 @@ Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 const char* ssid = "UNIFI_IDO1";
 const char* password = "42Bidules!";
+const char* apiToken = "a42485e1540c9b7f129254695a41d053";
 
 WebServer server(80);
+
+String fetchAndCleanData(const char* url, const char* fieldName) {
+  HTTPClient http;
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("X-Jsio-Token", apiToken);
+  
+  int httpCode = http.GET();
+  
+  if (httpCode == 200) {
+    String payload = http.getString();
+    
+    StaticJsonDocument<500> doc;
+    deserializeJson(doc, payload);
+    
+    if (doc.is<JsonArray>()) {
+      JsonObject obj = doc[0];
+      String value = obj[fieldName];
+      value.replace("INVALID_EXPRESSION: ", "");
+      return value;
+    } else if (doc.is<JsonObject>()) {
+      String value = doc[fieldName];
+      value.replace("INVALID_EXPRESSION: ", "");
+      return value;
+    }
+  }
+  return "Erreur";
+}
+
+void handleTemperature() {
+  String temp = fetchAndCleanData("https://api.jsonserver.io/donnees/temperature/collecter", "temperature");
+  server.send(200, "application/json", "{\"temperature\": " + temp + "}");
+}
+
+void handleHumidity() {
+  String humidity = fetchAndCleanData("https://api.jsonserver.io/donnees/humidity/collecter", "humidity");
+  server.send(200, "application/json", "{\"humidity\": " + humidity + "}");
+}
+
+void handleLuminosity() {
+  String luminosity = fetchAndCleanData("https://api.jsonserver.io/donnees/luminosity/collecter", "luminosity");
+  server.send(200, "application/json", "{\"luminosity\": \"" + luminosity + "\"}");
+}
+
+void handleMouvement() {
+  String mouvement = fetchAndCleanData("https://api.jsonserver.io/donnees/mouvement/collecter", "mouvement");
+  server.send(200, "application/json", "{\"mouvement\": \"" + mouvement + "\"}");
+}
+
+void handleUserName() {
+  String name = fetchAndCleanData("https://api.jsonserver.io/mes/donnees/collecter", "name");
+  server.send(200, "application/json", "{\"name\": \"" + name + "\"}");
+}
+
+void handleUserDevice() {
+  String device = fetchAndCleanData("https://api.jsonserver.io/user/device/info", "device");
+  server.send(200, "application/json", "{\"device\": \"" + device + "\"}");
+}
+
+void handleUserEmail() {
+  String email = fetchAndCleanData("https://api.jsonserver.io/user/email/data", "email");
+  server.send(200, "application/json", "{\"email\": \"" + email + "\"}");
+}
 
 void handleRoot() {
   File file = SPIFFS.open("/index.html", "r");
@@ -47,7 +113,7 @@ void setup() {
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    if(count = 1)
+    if(count == 1)
     {
       oled.clearDisplay();
       oled.setTextSize(1);            // set text size
@@ -60,7 +126,7 @@ void setup() {
       oled.display(); 
       delay(5000);
     }
-    else if(count = 2)
+    else if(count == 2)
     {
       oled.clearDisplay();
       oled.setTextSize(1);            
@@ -111,6 +177,14 @@ void setup() {
   // Define routes
   server.on("/", handleRoot);
   server.serveStatic("/style.css", SPIFFS, "/style.css");
+
+  server.on("/api/temperature", handleTemperature);
+  server.on("/api/humidity", handleHumidity);
+  server.on("/api/luminosity", handleLuminosity);
+  server.on("/api/mouvement", handleMouvement);
+  server.on("/api/userName", handleUserName);
+  server.on("/api/userDevice", handleUserDevice);
+  server.on("/api/userEmail", handleUserEmail);
 
   // Start server
   server.begin();
